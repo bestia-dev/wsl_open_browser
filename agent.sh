89ec4ff7870efbc96465462b4fcc59cd08f7d8b1
Luciano Bestia <LucianoBestia@gmail.com>
2021-08-22 10:47:29 +0200

    for crates.io

---
+++ agent.sh
@@ -0,0 +1,3 @@
+SSH_AUTH_SOCK=/tmp/ssh-N6wauvUqeOcR/agent.7879; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=7880; export SSH_AGENT_PID;
+echo Agent pid 7880;
---
+++ automation_tasks_rs/agent.sh
@@ -0,0 +1,3 @@
+SSH_AUTH_SOCK=/tmp/ssh-zpFWD3VZrzfq/agent.11978; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=11979; export SSH_AGENT_PID;
+echo Agent pid 11979;
