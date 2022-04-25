b18a48bb6c6576aa83507acf47ea3d6d0d7a1148
Luciano <luciano.bestia@gmail.com>
2022-04-25 12:49:27 +0200

    author

--- automation_tasks_rs/src/main.rs
+++ automation_tasks_rs/src/main.rs
@@ -142,7 +142,7 @@ fn task_docs() {
     auto_md_to_doc_comments();
     #[rustfmt::skip]
     let shell_commands = [
-        "cargo doc --no-deps --document-private-items --open",        
+        "cargo doc --no-deps --document-private-items",        
         // copy target/doc into docs/ because it is github standard
         "rsync -a --info=progress2 --delete-after target/doc/ docs/",
         "echo Create simple index.html file in docs directory",
